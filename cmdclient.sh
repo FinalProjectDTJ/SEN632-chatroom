2739dfc903f966abfa8e5b0763ed45f76cac4236
gaotian99 <gaotian99@hotmail.com>
2018-01-12 08:25:35 -0800

    Create cmdclient.sh

---
+++ cmdclient.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+
+# Color
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
+
+echo -e "${green} Starting Command Line Client ..."
+java -cp . client.ChatroomClient
