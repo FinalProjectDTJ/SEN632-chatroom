85f16d85835ffdeb83efc751f793febd041a8e06
gaotian99 <gaotian99@hotmail.com>
2018-01-12 09:04:21 -0800

    Update cmdclient.sh

--- cmdclient.sh
+++ cmdclient.sh
@@ -6,5 +6,5 @@ green='\033[0;32m'
 yellow='\033[0;33m'
 plain='\033[0m'
 
-echo -e "${green} Starting Command Line Client ..."
+echo -e "${green} Starting Command Line Client ... ${plain} "
 java -cp . client.ChatroomClient
