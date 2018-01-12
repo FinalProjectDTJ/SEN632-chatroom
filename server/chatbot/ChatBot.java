6ac6322d81e3cee27a0b68ec16153ffc79285f5f
gaotian99 <gaotian99@hotmail.com>
2018-01-11 22:38:19 -0800

    Update ChatBot.java

--- server/chatbot/ChatBot.java
+++ server/chatbot/ChatBot.java
@@ -1,4 +1,14 @@
-package server.chatbot
-public class ChatBot
-{
+package server.chatbot;
+
+//import java.io.InputStream;
+
+public class Chatbot {
+
+  public Chatbot() {
+    System.out.print("Chatbot ");
+  }
+
+  public static void loading() {
+    System.out.println("Chatbot loaded.");
+  }
 }
