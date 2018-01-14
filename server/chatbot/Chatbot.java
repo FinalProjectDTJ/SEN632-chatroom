b6305a835dcdfb8426cb2eb12a5b02d9b9f4b72b
gaotian99 <farsky@nulab.gtfix.com>
2018-01-13 22:53:03 -0500

    changes

--- server/chatbot/Chatbot.java
+++ server/chatbot/Chatbot.java
@@ -8,7 +8,12 @@ public class Chatbot {
   private static String[] jokes = new String[] {"\nMan said to God --- Why did you make women so beautiful?\nGod said to man --- So that you will love them.\nMan said to God --- But why did you make them so dumb?\nGod said to man --- So that they will love you.",
     "\nDid you hear about the skeleton who walked into a cafe?\nHe ordered a cup of coffee and a mop.",
     "\nMary: John says I'm pretty. Andy says I'm ugly. What do you think, Peter?\nPeter: I think you're pretty ugly.",
-    "\n\"Do you know what really amazes me about you?\"\n\"No.What?\"\n\"Oops.Sorry. I was thinking about someone else!\" "}; 
+    "\n\"Do you know what really amazes me about you?\"\n\"No.What?\"\n\"Oops.Sorry. I was thinking about someone else!\" ",
+    "\nIf tin whistles are made of tin, what are fog horns made of? ",
+    "\nIf vegetarians eat vegetables, what do humanitarians eat? ",
+    "\nA person who speaks two languages is bilingual...A person who speaks three languages is trilingual...A person who speaks four or more languages is multilingual. \nWhat is a person who speaks one language?\n  An American. ",
+    "\nWhy do we park our car in the driveway and drive our car on the parkway? "
+    }; 
 
   public Chatbot() {
 
@@ -47,7 +52,7 @@ public class Chatbot {
       if (subString[0].equals("help")) {
         return "fight cute cheers";
       }
-      return "No such things ...";
+      return "Unkown expression, no such things ...";
     }
 
     if (input.startsWith("@joke")) {
